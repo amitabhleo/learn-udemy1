8ca9b4f2e37a90c1c8c8abe86f25cf753e6163ab
amitabhleo@gmail.com <amitabhleo@gmail.com>
2019-03-16 10:35:14 +0530

    added a conditional operator

---
+++ HtmlLearn2/advance.js
@@ -0,0 +1,6 @@
+function isUserValid(bool){
+    return bool};
+    var answer = isUserValid(false) ? "you may enter !":"access denied !";
+
+    var automatedAnswer =
+        "your Account # is"+ (isUserValid(false) ? "1234":"Invalid Account");
