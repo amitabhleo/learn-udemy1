b0c8fc85a5a4db148442ee6e3e8d4994e937e038
amitabhleo@gmail.com <amitabhleo@gmail.com>
2019-03-16 12:18:16 +0530

    switch Case test

--- HtmlLearn2/advance.js
+++ HtmlLearn2/advance.js
@@ -1,6 +1,31 @@
-function isUserValid(bool){
+/*function isUserValid(bool){
     return bool};
     var answer = isUserValid(false) ? "you may enter !":"access denied !";
 
     var automatedAnswer =
         "your Account # is"+ (isUserValid(false) ? "1234":"Invalid Account");
+        */
+
+//Switch 
+
+function moveCommand(directon) {
+    var whatHappens;
+    switch (directon) {
+        case "forward":
+            whatHappens = "you encounter a monster";
+            break;
+        case "back":
+            whatHappens = "you reached home";
+            break;
+        case "left":
+            whatHappens = "you reached a river";
+            break;
+        case "right":
+            whatHappens = "you reached a hill";
+            break;
+        case "default":
+            whatHappens = "please enter a valid direction";
+            break;
+    }
+    return whatHappens;
+}
